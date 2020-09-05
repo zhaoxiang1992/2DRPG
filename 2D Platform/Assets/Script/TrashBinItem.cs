0b6ef142f45073c4358f5206964d7b8382f5ab7a
ZhaoXiang <1808724614@qq.com>
2020-09-06 00:01:08 +0800

    Add Game Sound Effects

---
+++ 2D Platform/Assets/Script/TrashBinItem.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashBinItem : MonoBehaviour
+{
+    private bool isPlayerInTrashBin;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            if (isPlayerInTrashBin)
+            {
+                if (CoinUI.CurrentCoinQuantity > 0)
+                {
+                    SoundManager.PlayTrowCoinClip();
+                    TrashBinCoin.coinCurrent++;
+                    CoinUI.CurrentCoinQuantity--;
+                }
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player") &&
+            other.GetType().ToString() == "UnityEngine.CapsuleCollider2D")
+        {
+            isPlayerInTrashBin = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player") &&
+            other.GetType().ToString() == "UnityEngine.CapsuleCollider2D")
+        {
+            isPlayerInTrashBin = false;
+        }
+    }
+}
