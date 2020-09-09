3c23b5bbd955c56b873d8592fe8edc22a01f304e
ZhaoXiang <1808724614@qq.com>
2020-09-09 21:26:23 +0800

    add pause menu and load screen

---
+++ 2D Platform/Assets/Script/MainMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class MainMenu : MonoBehaviour
+{
+    public GameObject loadingScreen;
+    public Slider slider;
+    public Text progressText;
+
+    //public void PlayGame()
+    //{
+    //    SceneManager.LoadScene(1);
+    //}
+
+    public void LoadLevel(int sceneIndex)
+    {
+        StartCoroutine(AsyncLoadLevel(sceneIndex));
+    }
+
+    IEnumerator AsyncLoadLevel(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        loadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = operation.progress / 0.9f;
+            slider.value = progress;
+            progressText.text = Mathf.FloorToInt(progress * 100f).ToString() + "%";
+            yield return null;
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
