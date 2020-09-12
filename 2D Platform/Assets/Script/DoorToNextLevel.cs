7358624356d7c337dd0b7cbf72e2d4dbfbe1632f
ZhaoXiang <1808724614@qq.com>
2020-09-12 22:46:46 +0800

    fix project

--- 2D Platform/Assets/Script/DoorToNextLevel.cs
+++ 2D Platform/Assets/Script/DoorToNextLevel.cs
@@ -15,7 +15,7 @@ public class DoorToNextLevel : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.R))
         {
             if (isInDoor)
             {
